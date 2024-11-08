4f470093333faa3bb9ca62c0df42e0438cf5d50a
Cheru <dennocheruh@gmail.com>
2024-11-08 17:44:21 +0300

    Add fibonacci function
    Added a fibonacci function to calculate the nth number
    of the fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
 
+    if (typeof n !== 'number'){
+        count = parseInt(countArg);
+    } else{
+        count = countArg;
+    }
+    if (count === 0) return 0;
+    if (count === 1) return 1;
+    if (count < 0) return "OOPS";
+    
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= count; i++){
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
